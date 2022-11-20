c84a3bfc2362dc83e8c7760c3655e34cd2c04ee1
Andy Chen <chen.andy18@gmail.com>
2022-11-20 14:47:15 -0800

    Time: 95 ms (51.09%), Space: 42.3 MB (36.69%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,18 +4,9 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[nums.length - 1] < target) {
-            return nums.length;
-        }
-        if (nums[0] > target) {
-            return 0;
-        }
-        if (nums[i] === target) {
-            return i;
-        }
-        if (nums[i] < target && nums[i + 1] > target) {
-            return i + 1;
-        }
-    }
+    let i = 0;
+    while (nums[i] < target) { 
+        i++;
+    };
+    return i;
 };

13d4c18ca32a8f902b1f2a7a93fae291fda532b2
Andy Chen <chen.andy18@gmail.com>
2022-11-21 17:04:01 -0800

    Time: 104 ms (29.57%), Space: 42 MB (50.07%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -20,8 +20,8 @@
 
 let plusOne = function(digits) {
     for (let i=digits.length - 1; i >= 0; i--) {
-        digits[i]++;
-        if(digits[i] > 9){
+        digits[i] += 1;
+        if(digits[i] > 9) {
             digits[i] = 0;
         }
         else{

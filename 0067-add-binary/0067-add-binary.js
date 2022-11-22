f4d9111b9f76dd7bd6459a0417b076d0e87cbb17
Andy Chen <chen.andy18@gmail.com>
2022-11-22 14:41:50 -0800

    Time: 119 ms (29.61%), Space: 41.9 MB (97.56%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -3,16 +3,10 @@
  * @param {string} b
  * @return {string}
  */
-// var addBinary = function(a, b) {
-    
-//     let newA = BigInt(`0b${a}`);
-//     let newB = BigInt(`0b${b}`);
-//     return Number(newA + newB).toString(2);
-// };
 
 var addBinary = function(a, b) {
-  const aBin = `0b${a}`
-  const bBin = `0b${b}`
-  const sum = BigInt(aBin) + BigInt(bBin)
+  const newA = `0b${a}`
+  const newB = `0b${b}`
+  const sum = BigInt(newA) + BigInt(newB)
   return sum.toString(2)
 };

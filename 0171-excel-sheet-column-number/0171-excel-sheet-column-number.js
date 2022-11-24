3ee78ba73b066ff4187b0ebecddabfce7c763eb6
Andy Chen <chen.andy18@gmail.com>
2022-11-24 15:47:17 -0800

    Time: 114 ms (59.86%), Space: 44 MB (38.58%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
@@ -3,9 +3,9 @@
  * @return {number}
  */
 var titleToNumber = function(columnTitle) {
-    let sum = 0
+    let sum = 0;
     for (let i = 0; i < columnTitle.length; i++) {
-        const exp = columnTitle.length - 1 - i
+        let exp = columnTitle.length - 1 - i
         sum += (columnTitle[i].charCodeAt(0) - 64) * (26 ** exp)
     }
     

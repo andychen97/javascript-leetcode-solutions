d7d6fb4055364925793c8f99b9c2347e1f1fdaf6
Andy Chen <chen.andy18@gmail.com>
2022-11-25 21:24:02 -0800

    Update 0125-valid-palindrome.js

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -5,7 +5,7 @@
 var isPalindrome = function(s) {
     let noSymbols = s.replace(/[^a-zA-Z0-9]/g, '');
     let lowered = noSymbols.toLowerCase();
-    let half = Math.floor(lowered.length / 2)
+    let half = Math.floor(lowered.length / 2);
     for (let i = 0; i < half; i++) {
         if (lowered[i] !== lowered[lowered.length - 1 - i]) {
             return false;

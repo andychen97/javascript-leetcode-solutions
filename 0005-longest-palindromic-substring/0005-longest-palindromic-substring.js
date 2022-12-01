b2224c38a642181af802db21d06ee0498a722349
Andy Chen <chen.andy18@gmail.com>
2022-12-01 11:47:44 -0800

    Time: 88 ms (96.05%), Space: 48.5 MB (35.11%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let max = [0, 1];
+    for(let i = 0; i < s.length; i++) {
+        let even = Palindrome(i - 1, i, s);
+        let odd = Palindrome(i - 1, i + 1, s);
+        let currentMax = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+        max = max[1] - max[0] > currentMax[1] - currentMax[0] ? max : currentMax;
+    }
+    return s.slice(max[0], max[1]);
+};
+
+const Palindrome = (left, right, s) => {
+    while (left >= 0 && right < s.length) {
+        if (s[left] !== s[right]) {
+            break;
+        }
+        left--;
+        right++;
+    }
+    return [left + 1, right]
+}

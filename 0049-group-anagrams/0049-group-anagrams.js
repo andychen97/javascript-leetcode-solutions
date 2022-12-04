0a7b1dea8643b4f03ccc69fa15bc67d597cb3044
Andy Chen <chen.andy18@gmail.com>
2022-12-04 15:38:23 -0800

    Time: 203 ms (66.92%), Space: 52.7 MB (76.73%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -10,8 +10,10 @@ var groupAnagrams = function(strs) {
     
     // loop through arguemnt to check if sorted in hash else push
     for (let i = 0; i < strs.length; i++) {
+        // create an array if not found
         if (!hash[sorted[i]]) {
             hash[sorted[i]] = [strs[i]];
+        // if found then push word into found array
         } else {
             hash[sorted[i]].push(strs[i]);
         }

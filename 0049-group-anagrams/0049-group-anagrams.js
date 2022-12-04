bb55cfc05cb5212795b4c84f6b96d22cbd67af68
Andy Chen <chen.andy18@gmail.com>
2022-12-04 15:35:56 -0800

    Time: 142 ms (85.37%), Space: 52.2 MB (90.02%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    //approach: hashmap based on sorted word by character
+    // map words to split by character, sort in ascending and join
+    const sorted = strs.map(word => word.split('').sort().join(''));
+    const hash = {};
+    
+    // loop through arguemnt to check if sorted in hash else push
+    for (let i = 0; i < strs.length; i++) {
+        if (!hash[sorted[i]]) {
+            hash[sorted[i]] = [strs[i]];
+        } else {
+            hash[sorted[i]].push(strs[i]);
+        }
+    }
+    // return values from object
+    return Object.values(hash);
+};

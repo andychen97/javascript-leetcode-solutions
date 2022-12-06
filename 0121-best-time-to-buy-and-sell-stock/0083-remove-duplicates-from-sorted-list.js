79fce55e870ca082c5dfc8fc073712e2d7ee722a
Andy Chen <chen.andy18@gmail.com>
2022-12-06 02:57:27 +0000

    Time: 72 ms (99.80%), Space: 51.5 MB (77.92%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0083-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let minPrice = prices[0];
+  let profit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else if (prices[i] - minPrice > profit) {
+      profit = prices[i] - minPrice;
+    }
+  }
+  return profit;
+};
+
+maxProfit([7, 1, 5, 3, 6, 4]);

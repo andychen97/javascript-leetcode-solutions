666cb22909871abec555c578f61c10612c297382
Andy Chen <chen.andy18@gmail.com>
2022-12-07 06:03:39 -0800

    Time: 102 ms (37.04%), Space: 42.8 MB (45.20%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -4,24 +4,14 @@
  */
 
 var uniqueOccurrences = function(arr) {
-    const obj = {};
-    
-    for(let el of arr) {
-        obj[el] = obj[el] ? ++obj[el] : 1;
+    let tracker = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (tracker[arr[i]]) {
+            tracker[arr[i]]++;
+        } else {
+            tracker[arr[i]] = 1;     
+        }
     }
-    const output = Object.values(obj);
-    
-    return output.length == new Set(output).size
+    const counter = Object.values(tracker);
+    return counter.length === new Set(counter).size;
 };
-
-// var uniqueOccurrences = function(arr) {
-//     let tracker = {};
-//     for (let i = 0; i < arr.length; i++) {
-//         if (tracker[arr[i]]) {
-//             tracker[arr[i]]++;
-//         }
-//         tracker[arr[i]] = 1;
-//     }
-//     const counter = Object.values(tracker);
-//     return counter.length === new Set(counter).size;
-// };

c9bf7fd174c253a0d9cbb05ee0fbced1e66c81a4
Andy Chen <chen.andy18@gmail.com>
2022-12-12 02:34:29 -0800

    Time: 55 ms (100.00%), Space: 42.3 MB (84.54%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.js
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.js
@@ -5,8 +5,7 @@
 var sumZero = function(n) {
     let arr =  n % 2 === 0 ? [] : [0];
     for (let i = 1; i < n; i += 2) {
-        arr.push(-i);
-        arr.push(i);  
+        arr.push(-i, i); 
     }
     return arr;
 };

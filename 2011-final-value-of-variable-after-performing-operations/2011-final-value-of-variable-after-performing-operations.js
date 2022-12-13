9f94d60075e3e2a95cdc22218176dc077554336e
Andy Chen <chen.andy18@gmail.com>
2022-12-13 05:07:09 -0800

    Time: 104 ms (44.50%), Space: 43.8 MB (17.43%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -4,8 +4,8 @@
  */
 var finalValueAfterOperations = function(operations) {
     let count = 0;
-    for (let i = 0; i < operations.length; i++) {
-        operations[i][1] === '-' ? count-- : count++;
+    for (elements of operations) {
+        elements[1] === '-' ? count-- : count++;
     } 
     return count;
 };

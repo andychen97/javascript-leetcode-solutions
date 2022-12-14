3e6ce1bd98c21cc1576c870abfc1561a685f6cc6
Andy Chen <chen.andy18@gmail.com>
2022-12-14 05:20:01 -0800

    Time: 59 ms (96.17%), Space: 42.2 MB (31.58%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
@@ -2,10 +2,16 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+
 var check = function(nums) {
-   var count = 0;
-    for(let i = 0; i < nums.length; i++) {
-        if(nums[i] > nums[(i+1) % nums.length]) count++;
+  let decreased = false;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < nums[i - 1]) {
+      if (decreased) {
+        return false;
+      }
+      decreased = true;
     }
-    return count > 1 ? false : true;
+  }
+  return decreased ? nums[0] >= nums[nums.length - 1] : true;
 };

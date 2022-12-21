07e85f66af38d17866d035bed76de01e7abf00bc
Andy Chen <chen.andy18@gmail.com>
2022-12-21 05:16:39 -0800

    Time: 105 ms (57.33%), Space: 44.5 MB (58.14%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    let shuffled = s.split('');
+    for (let i = 0; i < s.length; i++) {
+        shuffled[indices[i]] = s[i];
+    }
+    return shuffled.join('');
+};

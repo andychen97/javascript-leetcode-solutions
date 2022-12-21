dc310d36b44ca53f573a06e1765571d35670dab0
Andy Chen <chen.andy18@gmail.com>
2022-12-21 05:27:29 -0800

    Time: 97 ms (35.05%), Space: 42 MB (67.87%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.js
+++ 1816-truncate-sentence/1816-truncate-sentence.js
@@ -5,10 +5,5 @@
  */
 var truncateSentence = function(s, k) {
     if (k > s.length) return s;
-    let newSentence = [];
-    let splitted = s.split(' ');
-    for (let i = 0; i < k; i++) {
-        newSentence.push(splitted[i]);
-    }
-    return newSentence.join(' ');
+    return s.split(' ').splice(0, k).join(' ');
 };

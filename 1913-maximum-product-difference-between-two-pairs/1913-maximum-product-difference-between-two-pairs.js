092807efe7cc0577a4c9763a930478ce1866edcd
Andy Chen <chen.andy18@gmail.com>
2022-12-23 06:06:26 -0800

    Time: 165 ms (39.85%), Space: 44.7 MB (64.02%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
@@ -5,24 +5,24 @@
 
 //destructuring
 var maxProductDifference = function(nums) {
-    const [a, b] = nums.sort((a, b) => b - a)
-    const [c, d] = nums.reverse()
-    return (a * b) - (c * d)
+    const [a, b] = nums.sort((a, b) => b - a);
+    const [c, d] = nums.reverse();
+    return (a * b) - (c * d);
 };
 
-// brute force
-var maxProductDifference = function(nums) {
-    let highest = 0;
-    let lowest = Infinity;
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] * nums[j] > highest) {
-                highest = nums[i] * nums[j];
-            }
-            if (nums[i] * nums[j] < lowest) {
-                lowest = nums[i] * nums[j];
-            }
-        }
-    }
-    return highest - lowest;
-};
+// // brute force
+// var maxProductDifference = function(nums) {
+//     let highest = 0;
+//     let lowest = Infinity;
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] * nums[j] > highest) {
+//                 highest = nums[i] * nums[j];
+//             }
+//             if (nums[i] * nums[j] < lowest) {
+//                 lowest = nums[i] * nums[j];
+//             }
+//         }
+//     }
+//     return highest - lowest;
+// };

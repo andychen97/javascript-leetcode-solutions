f842ec878387c66e830629c6f1a0c25b43ae3367
Andy Chen <chen.andy18@gmail.com>
2022-12-23 06:03:38 -0800

    Time: 104 ms (78.53%), Space: 44.8 MB (55.13%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
@@ -2,18 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var maxProductDifference = function(nums) {
+//     let highest = 0;
+//     let lowest = Infinity;
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] * nums[j] > highest) {
+//                 highest = nums[i] * nums[j];
+//             }
+//             if (nums[i] * nums[j] < lowest) {
+//                 lowest = nums[i] * nums[j];
+//             }
+//         }
+//     }
+//     return highest - lowest;
+// };
+
 var maxProductDifference = function(nums) {
-    let highest = 0;
-    let lowest = Infinity;
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] * nums[j] > highest) {
-                highest = nums[i] * nums[j];
-            }
-            if (nums[i] * nums[j] < lowest) {
-                lowest = nums[i] * nums[j];
-            }
-        }
-    }
-    return highest - lowest;
+    const [a, b] = nums.sort((a, b) => b - a)
+    const [c, d] = nums.reverse()
+    return (a * b) - (c * d)
 };

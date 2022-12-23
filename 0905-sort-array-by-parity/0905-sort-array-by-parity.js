2187975b2c7a3470d1b78284e12c2531399ae72d
Andy Chen <chen.andy18@gmail.com>
2022-12-23 06:15:46 -0800

    Time: 115 ms (64.61%), Space: 44.8 MB (43.73%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    let arr = [];
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] % 2 === 0 ? arr.unshift(nums[i]) : arr.push(nums[i])
+    }
+    return arr;
+};

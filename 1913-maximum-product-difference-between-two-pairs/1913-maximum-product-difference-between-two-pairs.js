006db7db00b7c79cfc9f0d623d8bf9b8e111d83e
Andy Chen <chen.andy18@gmail.com>
2022-12-23 06:00:54 -0800

    Time: 781 ms (5.03%), Space: 44.8 MB (55.13%) - LeetHub

---
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProductDifference = function(nums) {
+    let highest = 0;
+    let lowest = Infinity;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] * nums[j] > highest) {
+                highest = nums[i] * nums[j];
+            }
+            if (nums[i] * nums[j] < lowest) {
+                lowest = nums[i] * nums[j];
+            }
+        }
+    }
+    return highest - lowest;
+};

b1133d4f574206923498735cf7342018eb5d7243
Andy Chen <chen.andy18@gmail.com>
2022-12-25 03:47:58 -0800

    Time: 146 ms (81.47%), Space: 60.8 MB (69.18%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+  var map = {};
+  for (var i = 0; i < nums.length; i++) {
+    if (map[nums[i]] >= 0 && i - map[nums[i]] <= k) {
+        return true;
+    } else {
+        map[nums[i]] = i;   
+    }
+  }
+  return false;
+};

dcc4caca42e630f38ff9080b771471b2c23b81d5
Andy Chen <chen.andy18@gmail.com>
2022-12-27 05:41:51 -0800

    Time: 176 ms (89.28%), Space: 50 MB (94.75%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,22 +2,11 @@
  * @param {number} x
  * @return {boolean}
  */
-
-'123321'
-length = 6
-index = 0 - 5
-
-'121'
-length = 3
-index = 0 - 2
-
 var isPalindrome = function(x) {
-    const string = x.toString();
-    const half  = Math.floor(string.length - 1 / 2);
+    let string = x.toString();
+    let half = Math.floor(string.length - 1 / 2);
     for (let i = 0; i < half; i++) {
-        if (string[i] !== string[string.length - 1 - i]) {
-            return false;
-        }
+        if (string[i] !== string[string.length - 1 - i]) return false;
     }
     return true;
 };

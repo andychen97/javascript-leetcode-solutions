b6dbdfa18deb77f10c8e754e90bcbcfb4c1aa7d8
Andy Chen <chen.andy18@gmail.com>
2022-12-28 10:02:19 -0800

    Time: 68 ms (95.21%), Space: 43.7 MB (73.88%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -6,7 +6,6 @@
 var findTheDifference = function(s, t) {
     let sNew = s.split('').sort();
     let tNew = t.split('').sort();
-    console.log(sNew, tNew)
     for (let i = 0; i < tNew.length; i++) {
         if (sNew[i] !== tNew[i]) return tNew[i];
     }

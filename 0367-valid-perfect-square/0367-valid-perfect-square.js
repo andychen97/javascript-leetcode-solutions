45e91cf2cb16c2fca8b093502642614a90c2eff3
Andy Chen <chen.andy18@gmail.com>
2022-12-28 09:36:47 -0800

    Time: 73 ms (75.90%), Space: 41 MB (100.00%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.js
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -3,7 +3,5 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    let possible = Number(Math.sqrt(num)) % 1 === 0;
-    
-    return possible;
+    return Number(Math.sqrt(num)) % 1 === 0;
 };

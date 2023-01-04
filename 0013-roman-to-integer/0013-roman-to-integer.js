ed17befa27c545b1773e0f2c3c9f2783ce92e35e
Andy Chen <chen.andy18@gmail.com>
2023-01-03 21:06:02 -0800

    Time: 191 ms (61.48%), Space: 46.8 MB (71.39%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -3,11 +3,13 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    const values = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000};
+    const roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
     let count = 0;
     for (let i = 0; i < s.length; i++) {
-        if (values[s[i]] < values[s[i + 1]]) count -= values[s[i]];
-        else count += values[s[i]];
+        let first = roman[s[i]];
+        let next = roman[s[i + 1]];
+        first < next ? count -= first : count += first;
     }
     return count;
-};
+}
+

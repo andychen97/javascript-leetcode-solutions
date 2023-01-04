375f683a91f18f7172fa35c26f809af23c6640ec
Andy Chen <chen.andy18@gmail.com>
2023-01-03 21:04:46 -0800

    Time: 225 ms (62.67%), Space: 51 MB (60.64%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,11 +2,14 @@
  * @param {number} x
  * @return {boolean}
  */
+
 var isPalindrome = function(x) {
-    let string = x.toString();
-    let half = Math.floor(string.length - 1 / 2);
+    const string = x.toString();
+    const half  = Math.floor(string.length - 1 / 2);
     for (let i = 0; i < half; i++) {
-        if (string[i] !== string[string.length - 1 - i]) return false;
+        if (string[i] !== string[string.length - 1 - i]) {
+            return false;
+        }
     }
     return true;
 };

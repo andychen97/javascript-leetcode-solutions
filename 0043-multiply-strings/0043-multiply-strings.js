417b25b391fbdc8dfea3643c0f06c8c568f284f4
Andy Chen <chen.andy18@gmail.com>
2023-01-04 05:00:23 -0800

    Time: 79 ms (78.50%), Space: 43.7 MB (62.85%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.js
+++ 0043-multiply-strings/0043-multiply-strings.js
@@ -4,5 +4,19 @@
  * @return {string}
  */
 var multiply = function(num1, num2) {
-    return String(BigInt(num1) * BigInt(num2));
+    if (num1 === '0' || num2 === '0') return '0'
+
+    const product = Array(num1.length + num2.length).fill(0);
+    let currIdx = product.length - 1;
+    
+    for (let i = num1.length-1; i >= 0; i--) {
+        let idx = currIdx--;
+        
+        for (let j = num2.length - 1; j >= 0; j--) {
+            const res = +num1[i] * +num2[j] + product[idx];
+            product[idx] = res % 10;
+            product[--idx] += Math.floor(res / 10);
+        }
+    }
+    return product.join('').replace(/^0+/, '')
 };

4d216c685c21346742044bf35979251c848dac76
Andy Chen <chen.andy18@gmail.com>
2023-01-04 05:33:02 -0800

    Time: 333 ms (45.10%), Space: 58.7 MB (70.59%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.js
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.js
@@ -10,15 +10,27 @@ var minimumRounds = function(tasks) {
       if (map[element]) map[element] += 1;
       else map[element] = 1;
     }
-    // let totalCount = Object.values(map);
     for (totalCount of Object.values(map)) {
         if (totalCount < 2) return -1;
         if (totalCount % 3 === 0) count += totalCount / 3;
         else count += Math.ceil(totalCount / 3);
-    // for (let i = 0; i < totalCount.length; i++) {
-        // if (totalCount[i] < 2) return -1;
-        // if (totalCount[i] % 3 === 0) count += totalCount[i] / 3;
-        // else count += Math.ceil(totalCount[i] / 3);
     }
     return count;
 };
+
+// SLOOOWWWW runtime so refactored to use for of loop instead of for loop
+// var minimumRounds = function(tasks) {
+//     const map = {};
+//     let count = 0;
+//     for (const element of tasks) {
+//       if (map[element]) map[element] += 1;
+//       else map[element] = 1;
+//     }
+//     let totalCount = Object.values(map);
+//     for (let i = 0; i < totalCount.length; i++) {
+//         if (totalCount[i] < 2) return -1;
+//         if (totalCount[i] % 3 === 0) count += totalCount[i] / 3;
+//         else count += Math.ceil(totalCount[i] / 3);
+//     }
+//     return count;
+// };

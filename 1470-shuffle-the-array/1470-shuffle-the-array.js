a116d4657b9ef16f41799130417d175d5af39670
Andy Chen <chen.andy18@gmail.com>
2023-01-11 07:43:52 -0800

    Time: 70 ms (90.87%), Space: 44.4 MB (50.72%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -6,8 +6,8 @@
 var shuffle = function(nums, n) {
     let newArr = [];
     for (let i = 0; i < n; i++) {
-        newArr.push(nums[i])
-        newArr.push(nums[i + n])
+        newArr.push(nums[i]);
+        newArr.push(nums[i + n]);
     }
     return newArr;
 };

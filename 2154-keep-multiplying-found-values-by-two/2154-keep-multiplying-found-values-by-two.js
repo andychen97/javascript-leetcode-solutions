2fca41a591d18e7bb842a3f1222f564830c61ca6
Andy Chen <chen.andy18@gmail.com>
2023-01-19 02:42:36 -0800

    Time: 79 ms (44.02%), Space: 44.3 MB (13.13%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
@@ -3,19 +3,18 @@
  * @param {number} original
  * @return {number}
  */
-// var findFinalValue = function(nums, original) {
-//     let possible = original;
-//     nums.sort((a,b) => a - b);
-//     for (let i = 0; i < nums.length; i++) {
-//         if (nums[i] === possible) possible *= 2;
-//     }
-//     return possible;
-// };
-
 var findFinalValue = function(nums, original) {
-    while (nums.includes(original)) {
-        original = original * 2
+    nums.sort((a,b) => a - b);
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === original) original *= 2;
     }
-    
-    return original
+    return original;
 };
+
+// var findFinalValue = function(nums, original) {
+//     while (nums.includes(original)) {
+//         original = original * 2
+//     }
+    
+//     return original
+// };

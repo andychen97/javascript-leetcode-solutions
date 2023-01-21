3e0edc11c73648dae0ca6080265db4a53ee6bec3
Andy Chen <chen.andy18@gmail.com>
2023-01-21 04:30:00 -0800

    Time: 88 ms (75.57%), Space: 43 MB (81.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -5,7 +5,25 @@
  */
 var isAnagram = function(s, t) {
     if (s.length !== t.length) return false;
-    let split = s.split('').sort().join('');
-    let tsplit = t.split('').sort().join('');
-    return split === tsplit;
+    let ss = {};
+    let ts = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (ss[s[i]]) {
+            ss[s[i]]++;
+        } else {
+            ss[s[i]] = 1;
+        }
+        if (ts[t[i]]) {
+            ts[t[i]]++;
+        } else {
+            ts[t[i]] = 1;
+        }
+    }
+    for (let key in ss) {
+        if (ss[key] !== ts[key]) {
+            return false;
+        }
+    }
+    return true;
 };

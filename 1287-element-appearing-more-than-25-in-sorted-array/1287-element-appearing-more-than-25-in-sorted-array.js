03a99b6cd5c46a7705dea2e699358886d9e57beb
Andy Chen <chen.andy18@gmail.com>
2023-01-30 18:17:21 -0800

    Time: 76 ms (71.78%), Space: 42.8 MB (71.78%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
@@ -2,14 +2,10 @@
  * @param {number[]} arr
  * @return {number}
  */
+
 var findSpecialInteger = function(arr) {
-    let map = {};
-    for (element of arr) {
-        map[element] ? map[element]++ : map[element] = 1;
+    let min = Math.floor(arr.length / 4);
+    for (let i = 0; i < arr.length - min; i++) {
+        if (arr[i] === arr[i + min]) return arr[i];
     }
-    
-    let count = arr.length;
-    for (key in map) {
-        if (map[key] / count > 0.25) return parseInt(key);
-    }
-};
+}

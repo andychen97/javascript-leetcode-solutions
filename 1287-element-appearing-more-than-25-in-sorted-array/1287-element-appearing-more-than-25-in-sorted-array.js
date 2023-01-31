3f9ae7701537e3470493519483c4dc8bf9468952
Andy Chen <chen.andy18@gmail.com>
2023-01-30 18:18:18 -0800

    Time: 76 ms (71.78%), Space: 49.2 MB (22.70%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
@@ -3,9 +3,24 @@
  * @return {number}
  */
 
+// fastest solution
 var findSpecialInteger = function(arr) {
     let min = Math.floor(arr.length / 4);
     for (let i = 0; i < arr.length - min; i++) {
         if (arr[i] === arr[i + min]) return arr[i];
     }
 }
+
+
+// My first solution
+var findSpecialInteger = function(arr) {
+    let map = {};
+    for (element of arr) {
+        map[element] ? map[element]++ : map[element] = 1;
+    }
+    
+    let count = arr.length;
+    for (key in map) {
+        if (map[key] / count > 0.25) return parseInt(key);
+    }
+};

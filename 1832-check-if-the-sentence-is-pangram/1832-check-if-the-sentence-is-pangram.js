574e06087ae0f07eafb8721ec61c2e54248e09c8
Andy Chen <chen.andy18@gmail.com>
2023-02-01 19:54:37 -0800

    Time: 72 ms (54.21%), Space: 43.1 MB (39.17%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
@@ -5,11 +5,7 @@
 var checkIfPangram = function(sentence) {
     let map = {};
     for (let i = 0; i < sentence.length; i++) {
-        if (map[sentence[i]] !== undefined) {
-            map[sentence[i]]++;
-        } else {
-            map[sentence[i]] = 1;
-        }
+        map[sentence[i]] ? map[sentence[i]]++ : map[sentence[i]] = 1;
     }
     return Object.values(map).length === 26;
 };

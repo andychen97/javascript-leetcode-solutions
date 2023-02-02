58d1008f623d3db31dd543a86a147431bb3b42d0
Andy Chen <chen.andy18@gmail.com>
2023-02-01 19:35:29 -0800

    Time: 63 ms (78.58%), Space: 41.5 MB (84.25%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -5,9 +5,9 @@
 var numIdenticalPairs = function(nums) {
     let count = 0;
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; j++) {
-            if (nums[i] == nums[j] && i < j) {
-                count++
+        for (let j = 1 + i; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                count++;
             }
         }
     }

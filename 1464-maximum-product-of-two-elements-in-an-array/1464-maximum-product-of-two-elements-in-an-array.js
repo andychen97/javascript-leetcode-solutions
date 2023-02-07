573d58328fd40da95476505a3ecbb9f43ee12f71
Andy Chen <chen.andy18@gmail.com>
2023-02-06 18:27:58 -0800

    Time: 67 ms (74.81%), Space: 42.2 MB (71.11%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -11,12 +11,12 @@ var maxProduct = function(nums) {
 
 
 //brute force
-var maxProduct = function(nums) {
-    let highest = 0;
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if ((nums[i]-1)*(nums[j]-1) > highest) highest = (nums[i]-1)*(nums[j]-1);
-        }
-    }
-    return highest;
-};
+// var maxProduct = function(nums) {
+//     let highest = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if ((nums[i]-1)*(nums[j]-1) > highest) highest = (nums[i]-1)*(nums[j]-1);
+//         }
+//     }
+//     return highest;
+// };

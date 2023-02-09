09b7b9083e81d9b0fcb6769978d34903ec01becc
Andy Chen <chen.andy18@gmail.com>
2023-02-08 17:27:27 -0800

    Time: 71 ms (58.97%), Space: 42.1 MB (83.76%) - LeetHub

--- 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.js
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.js
@@ -3,25 +3,11 @@
  * @param {string[]} words
  * @return {boolean}
  */
-// var isPrefixString = function(s, words) {
-//     let count = 0;
-//     for (let i = 0; i < words.length; i++) {
-//         if (count === s.length) {
-//             return true;
-//         } else if (words[i] !== s.slice(count, words[i].length + count)) {
-//             return false;
-//         } else {
-//             count += words[i].length;
-//         }
-//     }
-//     return true;
-// };
-
-const isPrefixString = (s, w) => {
-    let tmp = '';
-    for (const e of w) {
-        tmp += e;
-        if (tmp == s) return true; 
+var isPrefixString = function(s, words) {
+    let tempString = '';
+    for (let word of words) {
+        tempString += word;
+        if (tempString === s) return true;
     }
     return false;
 };

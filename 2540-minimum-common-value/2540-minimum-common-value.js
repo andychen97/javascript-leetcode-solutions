5fa1fe3acb2278c5f27a38b3c9cbfa45c48b39a0
Andy Chen <chen.andy18@gmail.com>
2023-02-08 19:54:46 -0800

    Time: 81 ms (84.93%), Space: 49.3 MB (94.09%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.js
+++ 2540-minimum-common-value/2540-minimum-common-value.js
@@ -3,11 +3,18 @@
  * @param {number[]} nums2
  * @return {number}
  */
+
 var getCommon = function(nums1, nums2) {
-    for (let i = 0; i < nums1.length; i++) {
-       for (let j = 0; j < nums2.length; j++) {
-        if (nums1[i] === nums2[j]) return nums1[i];
-        } 
+
+    let i = 0, j = 0;
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] === nums2[j]) {
+            return nums1[i];
+        } else if (nums1[i] < nums2[j]) {
+            i++;
+        } else {
+            j++;
+        }
     }
-    return -1;
+    return -1;    
 };

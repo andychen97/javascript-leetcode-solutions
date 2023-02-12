677178da2ca3b7288ed821777ab16f340a332185
Andy Chen <chen.andy18@gmail.com>
2023-02-11 19:24:02 -0800

    Time: 78 ms (23.59%), Space: 42.3 MB (63.59%) - LeetHub

--- 0925-long-pressed-name/0925-long-pressed-name.js
+++ 0925-long-pressed-name/0925-long-pressed-name.js
@@ -3,14 +3,6 @@
  * @param {string} typed
  * @return {boolean}
  */
-// var isLongPressedName = function(name, typed) {
-//    if (name[name.length - 1] !== typed[typed.length - 1]) return false;
-//     let ind = 0;
-//     for (let letter of typed) {
-//         if (letter === name[ind]) ind++;
-//    }
-//     return ind === name.length;
-// };
 
 var isLongPressedName = function(name, typed) {
     let i = 0;
@@ -20,6 +12,5 @@ var isLongPressedName = function(name, typed) {
         else if (typed[j] !== typed[j - 1]) return false;
         j++;
     }
-
     return i === name.length;
 };

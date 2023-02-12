d6e89eeb1aea889c12f864ad869a655d102224ae
Andy Chen <chen.andy18@gmail.com>
2023-02-11 17:48:43 -0800

    Time: 78 ms (23.44%), Space: 42.6 MB (26.30%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseOnlyLetters = function(s) {
+    let tempString = '';
+    for (let i = 0; i < s.length; i++) {
+        if (/[a-zA-Z]/.test(s[i])) tempString += s[i];
+    }
+
+    let reversed = tempString.split('').reverse().join('');
+    let newString = '';
+    let ind = 0;
+    for (let j = 0; j < s.length; j++) {
+        if (/[a-zA-Z]/.test(s[j])) {
+            newString += reversed[ind];
+            ind++;
+        } else {
+            newString += s[j];
+        }
+    }
+    return newString;
+};

70d979049a36a42d5c3e2c60f6eef050d44cc774
Andy Chen <chen.andy18@gmail.com>
2023-02-12 14:11:44 -0800

    Time: 60 ms (96.62%), Space: 43.4 MB (55.83%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -7,17 +7,18 @@ var longestPalindrome = function(s) {
     for (let letter of s) {
         map[letter] ? map[letter]++ : map[letter] = 1;
     }
-    let values = Object.values(map).sort((a,b) => b - a);
+    // let values = Object.values(map).sort((a,b) => b - a);
     let count = 0;
     let single = 1;
-    for (let j = 0; j < values.length; j++) {
-        if (values[j] % 2 === 0) {
-            count += values[j];
-        } else if (values[j] % 2 === 1 && single < 2) {
-            count += values[j];
+    // for (let j = 0; j < values.length; j++) {
+    for (let num in map) {
+        if (map[num] % 2 === 0) {
+            count += map[num];
+        } else if (map[num] % 2 === 1 && single < 2) {
+            count += map[num];
             single++;
-        } else if (values[j] % 2 === 1) {
-            count += values[j] - 1;
+        } else if (map[num] % 2 === 1) {
+            count += map[num] - 1;
         }
     }
     return count;

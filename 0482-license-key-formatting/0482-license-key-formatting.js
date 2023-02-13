e18cdcd7b3e9d5ef368305b726149f9bc4ef1897
Andy Chen <chen.andy18@gmail.com>
2023-02-13 14:25:50 -0800

    Time: 60 ms (98.37%), Space: 45.4 MB (91.85%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.js
+++ 0482-license-key-formatting/0482-license-key-formatting.js
@@ -4,16 +4,12 @@
  * @return {string}
  */
 var licenseKeyFormatting = function(s, k) {
-    let upperS = s.toUpperCase();
-    let newString = '';
-    for (let el of upperS) {
-        if (/[a-zA-Z0-9]/.test(el)) newString += el;
-    }
-    let remainder = newString.length % k;
-    let arr = remainder > 0 ? [newString.slice(0, remainder)] : [];
+    let upperS = s.replaceAll('-', '').toUpperCase();
+    let remainder = upperS.length % k;
+    let arr = remainder > 0 ? [upperS.slice(0, remainder)] : [];
     let tempString = ''
-    for (let i = remainder; i < newString.length; i += k) {
-        arr.push(newString.slice(i, i+k));
+    for (let i = remainder; i < upperS.length; i += k) {
+        arr.push(upperS.slice(i, i+k));
     }
     return arr.join('-');
 };

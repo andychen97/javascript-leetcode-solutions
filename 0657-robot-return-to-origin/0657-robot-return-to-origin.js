5f69082fd1521e801255346ddb9ca7fdc717c45a
Andy Chen <chen.andy18@gmail.com>
2023-02-15 13:46:51 -0800

    Time: 83 ms (37.24%), Space: 42.6 MB (83.28%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.js
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.js
@@ -3,12 +3,13 @@
  * @return {boolean}
  */
 var judgeCircle = function(moves) {
-    let count = 0;
+    let x = 0;
+    let y = 0;
     for (let i = 0; i < moves.length; i++) {
-        if (moves[i] === 'U') count++;
-        if (moves[i] === 'D') count--;
-        if (moves[i] === 'L') count += 5;
-        if (moves[i] === 'R') count -= 5;
+        if (moves[i] === 'U') x++;
+        if (moves[i] === 'D') x--;
+        if (moves[i] === 'L') y++;
+        if (moves[i] === 'R') y--;
     }
-    return count === 0;
+    return x === 0 && y === 0;
 };

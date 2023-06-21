d941533360c5c2b34cc74d93ae426b190268a9a5
Andy Chen <chen.andy18@gmail.com>
2023-06-21 16:34:11 -0700

    Time: 57 ms (64.58%), Space: 42 MB (57.00%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -2,14 +2,24 @@
  * @param {string} s
  * @return {number}
  */
+
 var lengthOfLastWord = function(s) {
-    if (s.length === 1) return 1;
-    const splitted = s.split(' ');
-    console.log(splitted);
-    for (let i = splitted.length - 1; i >= 0; i--) {
-        console.log(splitted[i])
-        if (splitted[i].length > 0) {
-            return splitted[i].length;
-        }
+    let count = 0;
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] === ' ' && count > 0) return count;
+        if (s[i] !== ' ') count++;
     }
+    return count;
 };
+
+// var lengthOfLastWord = function(s) {
+//     if (s.length === 1) return 1;
+//     const splitted = s.split(' ');
+//     console.log(splitted);
+//     for (let i = splitted.length - 1; i >= 0; i--) {
+//         console.log(splitted[i])
+//         if (splitted[i].length > 0) {
+//             return splitted[i].length;
+//         }
+//     }
+// };

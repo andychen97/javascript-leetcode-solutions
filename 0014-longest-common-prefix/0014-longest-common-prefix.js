a38830b39f29ddd714f1aa830914e2cfd59fa7b0
Andy Chen <chen.andy18@gmail.com>
2023-06-22 12:04:39 -0700

    Time: 63 ms (55.58%), Space: 42.1 MB (79.59%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,11 +3,16 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-  strs.sort();
-  for (let i = 0; i < strs[0].length; i++) {
-    if (strs[0][i] !== strs[strs.length - 1][i]) {
-      return strs[0].slice(0, i);
+    if (strs.length === 1) return strs[0];
+    if (strs.length === 1) return '';
+    
+    let common = '';
+    let sorted = strs.sort();
+    let shortest = Math.min(sorted[0].length, sorted[sorted.length - 1].length)
+    
+    for (let i = 0; i < shortest; i++) {
+        if (sorted[0][i] === sorted[sorted.length - 1][i]) common += sorted[0][i];
+        else return common;
     }
-  }
-  return strs[0];
+    return common;
 };

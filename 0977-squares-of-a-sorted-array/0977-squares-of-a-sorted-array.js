3e7b210d9e90c26d4a267ffe2de356b359c97566
Andy Chen <chen.andy18@gmail.com>
2023-06-29 16:19:42 -0700

    Time: 108 ms (46.70%), Space: 49 MB (40.10%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,8 +3,9 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
+    let arr = [];
     for (let i = 0; i < nums.length; i++) {
-        nums[i] *= nums[i];
+        arr.push(nums[i]**2);
     }
-    return nums.sort((a, b) => a - b);
+    return arr.sort((a, b) => a - b);
 };

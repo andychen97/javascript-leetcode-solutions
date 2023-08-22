371be9fcd04c70abb29607f0bccbd0d2ac2f7e0a
Andy Chen <chen.andy18@gmail.com>
2023-08-21 18:04:42 -0700

    Time: 46 ms (98.74%), Space: 43.1 MB (28.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -7,8 +7,8 @@
 var twoSum = function(nums, target) {
     let map = {};
     for (let i = 0; i < nums.length; i++) {
-        let possible = target - nums[i];
-        if (map[possible] !== undefined) return [i, map[possible]];
-        map[nums[i]] = i;
+        let temp = target - nums[i];
+        if (map[temp] === undefined) map[nums[i]] = i
+        else return [i, map[temp]]
     }
 };
